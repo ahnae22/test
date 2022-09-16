257d4bcaa0d902daa502fb0ad3de16734e8d6e1a
student <student@DB-507->
2022-09-16 21:54:26 +0800

    0914

---
+++ web0914/src/main/java/mvweb0914/FirstApp.java
@@ -0,0 +1,16 @@
+package mvweb0914;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("services")
+public class FirstApp extends Application {
+
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return new HashSet(Arrays.asList(HelloWorldRestService.class,Orders.class));
+    }
+}
